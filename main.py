ff2fa98726a63a5842852a76c9f738371fd1c2b9
Roman Andrushchenko <romanandrushchenko0@gmail.com>
2026-01-06 12:12:41 +0200

    Done all func

--- main.py
+++ main.py
@@ -1,111 +1,95 @@
-# Main file
-# Перевірка на наявність .json
-# Загружаємо файл
-# Очікуємо команди
-
 import time
 import json
 import os
 
-statuses = ('Created', 'In progress', 'Done')
-def create_task(): # Зроблено
-    with open(r"data/tasks.json", "r") as task_list:
-        task_list = json.load(task_list)
-    task_name = input("Enter a title of task: ")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+JSON_PATH = os.path.join(BASE_DIR, "data", "tasks.json")
+
+def open_json(): #Done
+    if not os.path.exists(JSON_PATH):
+        os.makedirs(os.path.dirname(JSON_PATH), exist_ok=True)
+        with open(JSON_PATH, "w") as data:
+            pattern = {"LAST ID": 0, "TASKS": []}
+            json.dump(pattern, data, indent=4)
+    with open(JSON_PATH, "r") as data:
+            return json.load(data)
+
+def save_json(json_file): #Done
+    with open(JSON_PATH, "w") as data:
+        json.dump(json_file, data, indent=4)
+        print("Succesfully!")
+
+def print_task(task): #Done
+    print(f"ID: {task["ID"]}")
+    print(f"Name: {task["Name"]}")
+    print(f"Status: {task["Status"]}")
+    print(f"Date: {task["Date"]}")
+    print(f"Last change: {task["Last Change"]}")
+    print("-" * 20)
+
+def search_task(id): #Done
+    tasks = open_json()
+    for task in tasks["TASKS"]:
+        if task["ID"] == id:
+            return task
+
+def list_tasks(): #Done
+    tasks = open_json()
+    print("-" * 20)
+    for task in tasks["TASKS"]:
+        print_task(task)
+        
+def create_task(): #Done
+    json_file = open_json()
+    task_name = input("> ")
     task_creating_time = time.strftime("%d/%m/%y %H:%M:%S")
-    task_id = len(task_list)
-    for task_names in task_list:
-        if task_list[task_names]["id"] == task_id:
-            task_id += 1
-    print(task_id)
-    task_list[task_name] = {
-                        "id": task_id,
-                        "status": 'Created',
-                        "date": task_creating_time,
-                        "done_time": None
-    }
-    with open(r'data/tasks.json', 'w+') as json_file:
-        json.dump(task_list, json_file, indent=4)
-    print(f'The task "{task_name}" successfully has been created!\n')
+    task_id = json_file["LAST ID"] + 1
+    json_file["TASKS"].append({
+                       "ID": task_id,
+                       "Name": task_name,
+                       "Status": "Created",
+                       "Date": task_creating_time,
+                       "Last Change": task_creating_time
+    })
+    json_file["LAST ID"] += 1
+    save_json(json_file)
 
-def delete_task(): # Зроблено
-    with open(r"data/tasks.json", "r") as task_list:
-        task_list = json.load(task_list)
-    del_id = int(input("Enter id of task, which would you like to delete: "))
-    print('Searching...')
-    for task_names in task_list:
-        if task_list[task_names]["id"] == del_id:
-            print(f'The task "{task_names}" has been succesfully deleted!\n')
-            del task_list[task_names]
-            break
-    else:
-        print(f'Task with {del_id} id has not been found...\n')
-    with open(r'data/tasks.json', 'w+') as json_file:
-        json.dump(task_list, json_file, indent=4)
+def delete_task(): # Done
+    json_file = open_json()
+    ch_id = int(input("> "))
+    json_file["TASKS"].remove(search_task(ch_id)) # Delete a task
+    save_json(json_file)
 
-def change_status(): # Зроблено
-     with open(r"data/tasks.json", "r") as task_list:
-        task_list = json.load(task_list)
-        chosen_id = int(input("Enter id of task, which would you like to edit: "))
-        chosen_status = input("Enter status of task(In progress/Done): ")
-        if chosen_status in statuses:
-            print('Searching...')
-            for task_names in task_list:
-                if task_list[task_names]["id"] == chosen_id:
-                    print(f'The task "{task_names}" status has been succesfully changed!\n')
-                    task_list[task_names]["status"] = chosen_status
-                    break
-            else:
-                print(f'Task with {chosen_id} id has not been found...\n')
-            with open(r'data/tasks.json', 'w+') as json_file:
-                json.dump(task_list, json_file, indent=4)
-        else:
-            print(f"You write a wrong status! '{chosen_status}' You can choose 'Created', 'In progress', 'Done'.")
+def change_task_status(): #Done
+    json_file = open_json()
+    ch_status = input("> ")
+    if ch_status not in ('Created', 'In progress', 'Done'):
+        print("ІДІ")
+        return None
+    ch_id = int(input("> "))
+    for task in json_file["TASKS"]:
+        if task["ID"] == ch_id:
+            task_change_time = time.strftime("%d/%m/%y %H:%M:%S")
+            task["Status"] = ch_status
+            task["Last Change"] = task_change_time
+    save_json(json_file)
 
-def show_tasks(): #Зроблено
-    print("Reading a file...")
-    with open(r"data/tasks.json", "r") as task_list:
-        task_list = json.load(task_list)
-    mode = input("Enter a mode(All, Created, In progress, Done) to show: ")
-    if mode == 'All':
-        print('-'*20)
-        for task_names in task_list:
-            print(f'Title: {task_names}')
-            print(f'Id: {task_list[task_names]["id"]}')
-            print(f'Status: {task_list[task_names]["status"]}')
-            print(f'Creating time: {task_list[task_names]["date"]}')
-            print(f'Done time: {task_list[task_names]["done_time"]}')
-            print('-'*20)
-    elif mode == 'Created':
-        print('-'*20)
-        for task_names in task_list:
-            if task_list[task_names]["status"] == 'Created':
-                print(f'Title: {task_names}')
-                print(f'Id: {task_list[task_names]["id"]}')
-                print(f'Status: {task_list[task_names]["status"]}')
-                print(f'Creating time: {task_list[task_names]["date"]}')
-                print(f'Done time: {task_list[task_names]["done_time"]}')
-                print('-'*20)
-    elif mode == 'In progress':
-        print('-'*20)
-        for task_names in task_list:
-            if task_list[task_names]["status"] == 'In progress':
-                print(f'Title: {task_names}')
-                princ(f'Id: {task_list[task_names]["id"]}')
-                print(f'Status: {task_list[task_names]["status"]}')
-                print(f'Creating time: {task_list[task_names]["date"]}')
-                print(f'Done time: {task_list[task_names]["done_time"]}')
-                print('-'*20)
-    elif mode == 'Done':
-        print('-'*20)
-        for task_names in task_list:
-            if task_list[task_names]["status"] == 'Done':
-                print(f'Title: {task_names}')
-                print(f'Id: {task_list[task_names]["id"]}')
-                print(f'Status: {task_list[task_names]["status"]}')
-                print(f'Creating time: {task_list[task_names]["date"]}')
-                print(f'Done time: {task_list[task_names]["done_time"]}')
-                print('-'*20)
-    else:
-        print("There is such a type of mode!")
-    
+def change_task_name(): #Done
+    json_file = open_json()
+    task_new_name = input("> ")
+    ch_id = int(input("> "))
+    for task in json_file["TASKS"]:
+        if task["ID"] == ch_id:
+            task_change_time = time.strftime("%d/%m/%y %H:%M:%S")
+            task["Name"] = task_new_name
+            task["Last Change"] = task_change_time
+    save_json(json_file)
+
+def filter_tasks(): #Done
+    json_file = open_json()
+    fil_status = input("> ")
+    if fil_status not in ('Created', 'In progress', 'Done'):
+        print("ІДІ")
+        return None
+    print("-" * 20)
+    [print_task(task) for task in json_file["TASKS"] if task["Status"] == fil_status]
