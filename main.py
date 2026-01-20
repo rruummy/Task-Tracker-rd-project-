832d5061aea9580692f5055dd81394398ef778fd
Roman Andrushchenko <romanandrushchenko0@gmail.com>
2026-01-20 23:12:01 +0200

    Procesing exception. Completed version.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import os
 import sys
 
-def open_json(): #Done
+def open_json():
     if not os.path.exists(JSON_PATH):
         os.makedirs(os.path.dirname(JSON_PATH), exist_ok=True)
         with open(JSON_PATH, "w") as data:
@@ -12,11 +12,11 @@ def open_json(): #Done
     with open(JSON_PATH, "r") as data:
             return json.load(data)
 
-def save_json(json_file): #Done
+def save_json(json_file):
     with open(JSON_PATH, "w") as data:
         json.dump(json_file, data, indent=4)
 
-def print_task(task): #Done
+def print_task(task):
     print(f"ID: {task["ID"]}")
     print(f"Name: {task["Name"]}")
     print(f"Description: {task["Description"]}")
@@ -25,22 +25,22 @@ def print_task(task): #Done
     print(f"Last change: {task["Last Change"]}")
     print("-" * 20)
 
-def search_task(id): #Done
+def search_task(id):
     tasks = open_json()
     for task in tasks["TASKS"]:
         if task["ID"] == id:
             return task
 
-def list_tasks(): #Done
+def list_tasks():
     tasks = open_json()
     print("-" * 20)
     for task in tasks["TASKS"]:
         print_task(task)
         
-def create_task(): #Done
+def create_task(name, desciption):
     json_file = open_json()
-    task_name = input("> ")
-    task_description = input("> ")
+    task_name = name
+    task_description = desciption
     task_creating_time = time.strftime("%d/%m/%y %H:%M:%S")
     task_id = json_file["LAST ID"] + 1
     json_file["TASKS"].append({
@@ -54,29 +54,36 @@ def create_task(): #Done
     json_file["LAST ID"] += 1
     save_json(json_file)
 
-def delete_task(): # Done
-    json_file = open_json()
-    ch_id = int(input("> "))
-    json_file["TASKS"].remove(search_task(ch_id)) # Delete a task
-    save_json(json_file)
+def delete_task(id):
+    try:
+        json_file = open_json()
+        ch_id = int(id)
+        json_file["TASKS"].remove(search_task(ch_id))
+        save_json(json_file)
+    except ValueError:
+        print(f"You wrote invalid id or there`s not such id. Use command 'list' to show all id.")
 
-def change_task_status(): #Done
+def change_task_status(id, status):
     json_file = open_json()
-    ch_status = input("> ")
-    if ch_status not in ('Created', 'In progress', 'Done'):
-        return None
-    ch_id = int(input("> "))
-    for task in json_file["TASKS"]:
-        if task["ID"] == ch_id:
-            task_change_time = time.strftime("%d/%m/%y %H:%M:%S")
-            task["Status"] = ch_status
-            task["Last Change"] = task_change_time
-    save_json(json_file)
+    ch_status = status
+    try:
+        if ch_status not in ('Created', 'In progress', 'Done'):
+            print("You wrote invalid status. ('Created', 'In progress', 'Done')")
+            return None
+        ch_id = int(id)
+        for task in json_file["TASKS"]:
+            if task["ID"] == ch_id:
+                task_change_time = time.strftime("%d/%m/%y %H:%M:%S")
+                task["Status"] = ch_status
+                task["Last Change"] = task_change_time
+                save_json(json_file)
+    except ValueError:
+        print(f"You wrote invalid id or there`s not such id. Use command 'list' to show all id.") 
 
-def change_task_name(): #Done
+def change_task_name(id, name):
     json_file = open_json()
-    task_new_name = input("> ")
-    ch_id = int(input("> "))
+    task_new_name = name
+    ch_id = int(id)
     for task in json_file["TASKS"]:
         if task["ID"] == ch_id:
             task_change_time = time.strftime("%d/%m/%y %H:%M:%S")
@@ -84,9 +91,9 @@ def change_task_name(): #Done
             task["Last Change"] = task_change_time
     save_json(json_file)
 
-def filter_tasks(): #Done
+def filter_tasks(status):
     json_file = open_json()
-    fil_status = input("> ")
+    fil_status = status
     if fil_status not in ('Created', 'In progress', 'Done'):
         print("There`s not such status.(Write Created/In progress/Done)")
         return None
@@ -96,31 +103,37 @@ def filter_tasks(): #Done
 def help():
     print("=" * 20)
     print("List of commands:")
-    print("create - to create new task")
-    print("delete - to delete task")
+    print("create 'name' 'descption' - to create new task")
+    print("delete 'id' - to delete task")
     print("list - show all tasks")
-    print("schange - change a status of task")
-    print("nchange - change a name of task")
-    print("filter - show filtered tasks")
+    print("schange 'id' 'Created/In progress/Done' - change a status of task")
+    print("nchange 'id' 'name' - change a name of task")
+    print("filter 'Created/In progress/Done'- show filtered tasks")
     print("=" * 20)
 
 if __name__ == "__main__":
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     JSON_PATH = os.path.join(BASE_DIR, "data", "tasks.json")
-    if sys.argv[1] == "list":
-        list_tasks()
-    elif sys.argv[1] == "create":
-        create_task()
-    elif sys.argv[1] == "delete":
-        delete_task()
-    elif sys.argv[1] == "schange":
-        change_task_status()
-    elif sys.argv[1] == "nchange":
-        change_task_name()
-    elif sys.argv[1] == "filter":
-        filter_tasks()
-    elif sys.argv[1] == "help":
-        help()
-    else:
-        print(f"\nThere's not such a command.")
+    try:
+        if sys.argv[1] == "list":
+            list_tasks()
+        elif sys.argv[1] == "create":
+            name = sys.argv[2]
+            description = sys.argv[3] if len(sys.argv) > 3 else None
+            create_task(name, description)
+        elif sys.argv[1] == "delete":
+            delete_task(sys.argv[2])
+        elif sys.argv[1] == "schange":
+            change_task_status(sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == "nchange":
+            change_task_name(sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == "filter":
+            filter_tasks(sys.argv[2])
+        elif sys.argv[1] == "help":
+            help()
+        else:
+            print(f"\nThere's not such a command.")
+            help()
+    except IndexError:
+        print("Write a command after 'py main.py'")
         help()
